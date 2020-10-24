e12314e0f2880b2a0847bf364324d6a73f3b4f3b
TJ <ivtjt78@gmail.com>
2020-10-24 17:31:23 -0400

    added startSensors script

--- rpm-sensor/src/main/java/Main.java
+++ rpm-sensor/src/main/java/Main.java
@@ -2,18 +2,19 @@ import java.rmi.RemoteException;
 
 import receiver.RPMBeatChecker;
 import sender.RPMSensor;
+import sender.TempSensorException;
 
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
-			RPMSensor rpmsensor = new RPMSensor();
-		} catch (RemoteException e) {
+			RPMSensor.main(args);
+		} catch (TempSensorException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		RPMBeatChecker rpmbc = new RPMBeatChecker();
+		RPMBeatChecker.main(args);
 
 	}
 
