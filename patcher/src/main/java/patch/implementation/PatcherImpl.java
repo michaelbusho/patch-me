7b710a9bacbd82ff825365cd28b87c1777c549ba
Mihal Busho <bushomichael@gmail.com>
2020-10-23 21:09:27 -0400

    create structure and add patching scripts

--- patcher/src/main/java/patch/implementation/PatcherImpl.java
+++ patcher/src/main/java/patch/implementation/PatcherImpl.java
@@ -3,6 +3,8 @@ package patch.implementation;
 import patch.Patcher;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class PatcherImpl implements Patcher {
     Process process = null;
@@ -32,9 +34,14 @@ public class PatcherImpl implements Patcher {
                 sb.append(" && echo Code Downloaded");
             }
 
-            ProcessBuilder pb = new ProcessBuilder("bash", "-c", sb.toString());
+//            Path currentRelativePath = Paths.get("");
+//            String s = currentRelativePath.toAbsolutePath().toString();
+//            System.out.println(s);
+//            ProcessBuilder pb = new ProcessBuilder("patch_jar.sh", "myArg1", "myArg2");
+
+            ProcessBuilder pb = new ProcessBuilder("pwd");
             pb.inheritIO();
-            pb.directory(new File(System.getProperty("user.home") +"/Desktop")); //Set current directory
+//            pb.directory(new File(System.getProperty("user.home") +"/Desktop")); //Set current directory
 //            pb.redirectError(new File(System.getProperty("user.home") +"/Desktop"));//Log errors in specified log file.
             process = pb.start();
             process.waitFor();
