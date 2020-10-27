64a7be3686b69f371c615bb9dd764ce4d9222be5
Mihal Busho <bushomichael@gmail.com>
2020-10-26 20:54:19 -0400

    new pacther impl

--- rpm-sensor/src/main/java/Main.java
+++ rpm-sensor/src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("--Hello World from the rpm-sensor");
+        System.out.println("--This is now updated code inside of the rpm-sensor");
+        System.out.println("--End of rpm sensor");
     }
 }
--- temp-sensor/src/main/java/Main.java
+++ temp-sensor/src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("--Hello World from the temp-sensor");
+        System.out.println("--This is now updated code inside of the temp-sensor");
+        System.out.println("--End of temp-sensor");
     }
 }
